d6bbcf32250e612a591f72c7867ce4c69e2509a4
alonsd <alonshuld@gmail.com>
2022-04-08 13:39:49 +0300

    client python V1.0.3 finished

--- clientPython/clientPython/clientPython.py
+++ clientPython/clientPython/clientPython.py
@@ -13,7 +13,41 @@ def print_recv(sock):
     input: socket
     return: none
     """
-    print(sock.recv(128).decode())
+    print("Recieved:    " + sock.recv(CODE_SIZE).decode(), end="")
+    len = sock.recv(LEN_OF_BITS).decode()
+    print(len + sock.recv(int(len)).decode())
+
+
+def send_signup(sock, username, password, email):
+    """
+    this function sends signup request
+    :param sock: socket that we send the message
+    :type sock: socket
+    :param username: the username of the new user
+    :type username: string
+    :param password: the password of the new user
+    :type password: string
+    :param email: the email of the new user
+    :type email: string
+    """
+    msg = json.loads('{"username": "' + username + '", "password": "' + password + '", "email": "' + email + '"}')
+    print("Send:    " + 'b' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)) # print the message we send
+    sock.sendall(str('b' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)).encode())
+
+
+def send_login(sock, username, password):
+    """
+    this function sends login request
+    :param sock: socket that we send the message
+    :type sock: socket
+    :param username: the username of the new user
+    :type username: string
+    :param password: the password of the new user
+    :type password: string
+    """
+    msg = json.loads('{"username": "' + username + '", "password": "' + password + '"}')
+    print("Send:    " + 'a' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)) # print the message we send
+    sock.sendall(str('a' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)).encode())
 
 
 def client_conv_with_server():
@@ -26,15 +60,17 @@ def client_conv_with_server():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (SERVER_IP, SERVER_PORT)
         sock.connect(server_address)
-        msg = json.loads('{"username": "user1", "password": "1234", "email":"user1@gmail.com"}')
-        print("Send:    " + 'b' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)) # print the message we send
-        sock.sendall(str('b' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)).encode())
+        send_login(sock, "Unknown", "tralala")      # login with user that has not signed up
         print_recv(sock)
-        msg = json.loads('{"username": "user1", "password": "1234"}')
-        print("Send:    " + 'a' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)) # print the message we send
-        sock.sendall(str('a' + str(len(json.dumps(msg))).rjust(LEN_OF_BITS, "0") + json.dumps(msg)).encode())
+        send_signup(sock, "user1", "pass1", "user1@gmail.com")     # regular signup
         print_recv(sock)
-    
+        send_signup(sock, "user1", "pass1", "user1@gmail.com")     # signup with user that already exists
+        print_recv(sock)
+        send_login(sock, "user1", "pass1")     # regular login
+        print_recv(sock)
+        send_login(sock, "user1", "pass1")     # login with user that is already loged in 
+        print_recv(sock)
+
     except Exception as e:
         sock.close()
         print("Error!: ", e)
