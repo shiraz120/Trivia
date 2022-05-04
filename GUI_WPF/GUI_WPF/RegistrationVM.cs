2862b945b2eb3e128f778d6e789f653912de0428
Shiraz Shiraz <shirazamos2@gmail.com>
2022-05-04 22:45:17 +0300

    added registrationVM

---
+++ GUI_WPF/GUI_WPF/RegistrationVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_WPF
+{
+    public class RegistrationVM : IDataErrorInfo
+    {
+        private string _username;
+        private bool _canSave;
+        public bool CanSave
+        {
+            get { return _canSave; }
+            set { _canSave = value; }
+        }
+        public string Error { get { return null; } }
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+            }
+        }
+        public string this[string name]
+        {
+            get
+            {
+                string result = null;
+                switch(name)
+                {
+                    case "Username":
+                        if (string.IsNullOrWhiteSpace(Username))
+                        {
+                            result = "user name must be implemented";
+                        }
+                        break;
+                }
+                CanSave = result == null;
+                return result;
+            }
+        }
+
+    }
+}
